a9e15c38f69bdbee5f56178bfd4f205a38488d61
laetitia123 <uwizelaetitia@gmail.com>
2019-07-26 12:23:04 +0200

    text added to the last button

--- js/script.js
+++ js/script.js
@@ -31,20 +31,101 @@ $(document).ready(function() {
 
       $(".port1").hover(function(){
         $(".port1").mouseenter(function(){
-        $("button").show();
+        $("#button").show();
         });
         $(".port1").mouseleave(function(){
-          $("button").hide();
+          $("#button").hide();
           });
          
       });
+      $(".port1").hover(function(){
+        $(".port1").mouseenter(function(){
+        $("#button").show();
+        });
+        $(".port1").mouseleave(function(){
+          $("#button").hide();
+          });
+         
+      });
+
+      $(".port2").hover(function(){
+        $(".port2").mouseenter(function(){
+        $("#button2").show();
+        });
+        $(".port2").mouseleave(function(){
+          $("#button2").hide();
+          });
+         
+      });
+
+      $(".port3").hover(function(){
+        $(".port3").mouseenter(function(){
+        $("#button3").show();
+        });
+        $(".port3").mouseleave(function(){
+          $("#button3").hide();
+          });
+         
+      });
+
+      $(".port4").hover(function(){
+        $(".port4").mouseenter(function(){
+        $("#button4").show();
+        });
+        $(".port4").mouseleave(function(){
+          $("#button4").hide();
+          });
+         
+      });
+      $(".port5").hover(function(){
+        $(".port5").mouseenter(function(){
+        $("#button5").show();
+        });
+        $(".port5").mouseleave(function(){
+          $("#button5").hide();
+          });
+         
+      });
+      $(".port6").hover(function(){
+        $(".port6").mouseenter(function(){
+        $("#button6").show();
+        });
+        $(".port6").mouseleave(function(){
+          $("#button6").hide();
+          });
+         
+      });
+      $(".port7").hover(function(){
+        $(".port7").mouseenter(function(){
+        $("#button7").show();
+        });
+        $(".port7").mouseleave(function(){
+          $("#button7").hide();
+          });
+         
+      });
+      $(".port8").hover(function(){
+        $(".port8").mouseenter(function(){
+        $("#button8").show();
+        });
+        $(".port8").mouseleave(function(){
+          $("#button8").hide();
+          });
+         
+      });
+
+      
+          
+         
+     
+
 
 
 $("#submit").click(function(){
 var name=$("#name").val();
 var email=$("#email").val();
 var textarea=$("styled").val();
-alert("hey " +name +"Thank you for contact us");
+alert("Hey " +name +"Thank you for contact us");
  
 });
 
